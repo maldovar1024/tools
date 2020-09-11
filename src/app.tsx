c4e223343c5c48e0cfbe0f29f3442589c2cdc651
Zhang Zhengyi <16307110079@fudan.edu.cn>
2020-09-11 15:36:01 +0800

    feat: 添加tab切换

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Tabs } from 'antd';
+import { RadixConverter } from './radix-converter';
+import './app.less';
+
+const { TabPane } = Tabs;
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Tabs tabPosition="left">
+        <TabPane tab="进制转换" key="radix-converter">
+          <RadixConverter />
+        </TabPane>
+      </Tabs>
+    </div>
+  );
 }
