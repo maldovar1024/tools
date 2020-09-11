47a9b79a2aea9c43c3485f077098d4b9c88ba44a
Zhang Zhengyi <16307110079@fudan.edu.cn>
2020-09-11 15:11:00 +0800

    feat: 添加进制转换组件

---
+++ src/radix-converter/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState, FC } from 'react';
+import { parseNumber } from './utils';
+import { RadixConverterShower } from './radix-converter-shower';
+
+export const RadixConverter: FC = () => {
+  const [inputRadix, setInputRadix] = useState(10);
+  const [resultRadix, setResultRadix] = useState(2);
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleInputRadixChange = (radix: number) => {
+    setInputRadix(radix);
+    setInput('');
+    setResult('');
+  };
+
+  const handleResultRadixChange = (radix: number) => {
+    const newResult = input !== '' ? parseNumber(input, inputRadix).toString(radix) : '';
+    setResult(newResult);
+    setResultRadix(radix);
+  };
+
+  const handleInputChange = (newInput: string) => {
+    if (newInput === '') {
+      setInput('');
+      setResult('');
+    } else {
+      const newResult = parseNumber(newInput, inputRadix).toString(resultRadix);
+      if (newResult !== 'NaN') {
+        setInput(newInput);
+        setResult(newResult);
+      }
+    }
+  };
+
+  return (
+    <div className="radix-converter">
+      <div className="title">
+        进制转换：
+        <span className="subtitle">支持 2~36 进制的整数及小数</span>
+      </div>
+      <div className="content">
+        <RadixConverterShower
+          className="input"
+          radix={inputRadix}
+          value={input}
+          inputMode={true}
+          addonBefore={'转换数字'}
+          onRadixChange={handleInputRadixChange}
+          onInputChange={handleInputChange}
+        />
+        <RadixConverterShower
+          className="result"
+          radix={resultRadix}
+          value={result}
+          inputMode={false}
+          addonBefore={'转换结果'}
+          onRadixChange={handleResultRadixChange}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/radix-converter/radix-converter-shower.tsx
@@ -0,0 +1,60 @@
+import React, { FC } from 'react';
+import { Input, Radio, Select } from 'antd';
+import { range } from './utils';
+
+const RadioGroup = Radio.Group;
+const Option = Select.Option;
+
+interface RadixConverterShowerCommonProp {
+  className?: string;
+  radix: number;
+  value: string;
+  addonBefore: string;
+  readonly?: boolean;
+  onRadixChange: (radix: number) => void;
+  onInputChange?: (input: string) => void;
+}
+
+type RadixConverterShowerProp = (
+  | { inputMode: true; onInputChange: (input: string) => void }
+  | { inputMode: false }
+) &
+  RadixConverterShowerCommonProp;
+
+const partRadixes = [2, 8, 10, 16]; // 单选框可用的进制
+const allRadixes = [...range(2, 37)]; // 全部进制
+
+export const RadixConverterShower: FC<RadixConverterShowerProp> = props => (
+  <div className={props.className}>
+    <div className="selection">
+      <RadioGroup value={props.radix} onChange={e => props.onRadixChange(e.target.value)}>
+        {partRadixes.map(radix => (
+          <Radio key={radix} value={radix}>
+            {radix} 进制
+          </Radio>
+        ))}
+      </RadioGroup>
+      <Select value={props.radix} onChange={props.onRadixChange} style={{ width: 90 }}>
+        {allRadixes.map(radix => (
+          <Option key={radix} value={radix}>
+            {radix} 进制
+          </Option>
+        ))}
+      </Select>
+    </div>
+    {props.inputMode ? (
+      <Input
+        value={props.value}
+        addonBefore={props.addonBefore}
+        allowClear
+        onChange={e => props.onInputChange(e.target.value)}
+      />
+    ) : (
+      <Input
+        value={props.value}
+        addonBefore={props.addonBefore}
+        readOnly={true}
+      />
+    )}
+  </div>
+);
