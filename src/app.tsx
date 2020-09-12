c5eab94d069c4b12447cb9702131b64562feb143
Zhang Zhengyi <16307110079@fudan.edu.cn>
2020-09-12 20:25:54 +0800

    feat: 初步实现浮点数转换功能

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Tabs } from 'antd';
 import { RadixConverter } from './radix-converter';
 import './app.less';
+import { FormConverter } from './form-converter';
 
 const { TabPane } = Tabs;
 
@@ -11,6 +12,9 @@ export default function App() {
       <Tabs tabPosition="left">
         <TabPane tab="进制转换" key="radix-converter">
           <RadixConverter />
+        </TabPane>
+        <TabPane tab="格式转换" key="form-converter">
+          <FormConverter />
         </TabPane>
       </Tabs>
     </div>
---
+++ src/form-converter/index.tsx
@@ -0,0 +1,70 @@
+import { Radio } from 'antd';
+import React, { useState, FC } from 'react';
+import { FloatShower } from './float-shower';
+import Label from './label';
+import { convertRepresentationOfNumber, FloatType } from './utils';
+
+const RadioGroup = Radio.Group;
+
+export const FormConverter: FC = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+  const [inputMode, setInputMode] = useState<FloatType>('f32');
+  const [resultMode, setResultMode] = useState<FloatType>('f32');
+
+  const handleInputChange = (input: string) => {
+    setInput(input);
+    const result = convertRepresentationOfNumber(input, inputMode, resultMode);
+    if (result !== null) {
+      setResult(result);
+    }
+  };
+
+  const handleInputModeChange = (mode: FloatType) => {
+    setInputMode(mode);
+    setInput('');
+    setResult('');
+  };
+
+  const handleResultModeChange = (mode: FloatType) => {
+    setResultMode(mode);
+    const result = convertRepresentationOfNumber(input, inputMode, mode) ?? '';
+    setResult(result);
+  };
+
+  return (
+    <div className="form-converter">
+      <div className="title">
+        格式转换：
+        <span className="subtitle">十进制数与 IEEE 754 32 位、64 位浮点数之间的转换</span>
+      </div>
+      <div className="content">
+        <div className="input">
+          <div className="selection">
+            <RadioGroup value={inputMode} onChange={e => handleInputModeChange(e.target.value)}>
+              <Radio value="f32">32 位浮点数</Radio>
+              <Radio value="f64">64 位浮点数</Radio>
+            </RadioGroup>
+          </div>
+          <Label>输入数字</Label>
+          <FloatShower
+            inputMode={true}
+            value={input}
+            onInputChange={handleInputChange}
+            floatType={inputMode}
+          />
+        </div>
+        <div className="result">
+          <div className="selection">
+            <RadioGroup value={resultMode} onChange={e => handleResultModeChange(e.target.value)}>
+              <Radio value="f32">32 位浮点数</Radio>
+              <Radio value="f64">64 位浮点数</Radio>
+            </RadioGroup>
+          </div>
+          <Label>转换结果</Label>
+          <FloatShower inputMode={false} value={result} floatType={resultMode} />
+        </div>
+      </div>
+    </div>
+  );
+};
