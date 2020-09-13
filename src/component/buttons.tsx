0960ce3ef67ffe60349e0471ecf767b155febd6b
Zhang Zhengyi <16307110079@fudan.edu.cn>
2020-09-13 19:21:18 +0800

    feat: 添加了清除按钮组件

---
+++ src/component/buttons.tsx
@@ -0,0 +1,32 @@
+import { CloseCircleFilled, CopyOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
+import React, { FC } from 'react';
+
+interface CopyButtonProp {
+  disabled: boolean;
+  onCopy: () => void;
+  tip: string;
+}
+
+export const CopyButton: FC<CopyButtonProp> = ({ disabled, onCopy, tip }) => (
+  <Tooltip title={tip} color="cyan">
+    <Button disabled={disabled} icon={<CopyOutlined />} onClick={onCopy} />
+  </Tooltip>
+);
+
+interface ClearButtonProp {
+  disabled: boolean;
+  onClick: React.MouseEventHandler<HTMLElement>;
+  tip: string;
+}
+
+export const ClearButton: FC<ClearButtonProp> = ({ disabled, onClick, tip }) => (
+  <Tooltip title={tip} color="cyan">
+    <Button
+      disabled={disabled}
+      style={{ color: 'rgba(0, 0, 0, 0.25)' }}
+      icon={<CloseCircleFilled />}
+      onClick={onClick}
+    />
+  </Tooltip>
+);
