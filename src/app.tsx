ceba5b52c04d2cdee2950f489d2a90d9080f506d
Zhang Zhengyi <16307110079@fudan.edu.cn>
2020-09-19 23:45:20 +0800

    feat: 添加路由

--- src/app.tsx
+++ src/app.tsx
@@ -1,26 +1,38 @@
+import { Layout, Menu } from 'antd';
 import React from 'react';
-import { Tabs } from 'antd';
-import { RadixConverter } from './radix-converter';
+import { Link, Route, Switch, useLocation } from 'react-router-dom';
 import './app.less';
 import { FormConverter } from './form-converter';
 import Introduction from './introduction';
+import { RadixConverter } from './radix-converter';
 
-const { TabPane } = Tabs;
+const { Sider, Content } = Layout;
+const { Item } = Menu;
 
-export default function App() {
+export default function App(): JSX.Element {
+  const location = useLocation();
   return (
-    <div className="App">
-      <Tabs tabPosition="left">
-        <TabPane tab="简介" key="introduction">
-          <Introduction />
-        </TabPane>
-        <TabPane tab="进制转换" key="radix-converter">
-          <RadixConverter />
-        </TabPane>
-        <TabPane tab="格式转换" key="form-converter">
-          <FormConverter />
-        </TabPane>
-      </Tabs>
-    </div>
+    <Layout className="App">
+      <Sider>
+        <Menu mode="inline" selectedKeys={[location.pathname]}>
+          <Item key="/">
+            <Link to="/">简介</Link>
+          </Item>
+          <Item key="/radix-converter">
+            <Link to="/radix-converter">进制转换</Link>
+          </Item>
+          <Item key="/form-converter">
+            <Link to="/form-converter">格式转换</Link>
+          </Item>
+        </Menu>
+      </Sider>
+      <Content>
+        <Switch>
+          <Route exact path="/" component={Introduction} />
+          <Route exact path="/radix-converter" component={RadixConverter} />
+          <Route exact path="/form-converter" component={FormConverter} />
+        </Switch>
+      </Content>
+    </Layout>
   );
 }
