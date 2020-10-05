de510391ecac1bc33e6fd5a9f3be6473e02b92e9
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-05 18:35:55 +0800

    style: 给 useConverterState 添加返回类型

--- src/hook.ts
+++ src/hook.ts
@@ -1,11 +1,22 @@
 import { Input } from 'antd';
-import { useRef, useState } from 'react';
+import { RefObject, useRef, useState } from 'react';
+
+interface ConverterStateHook<I, R> {
+  inputRef: RefObject<Input>;
+  input: string;
+  result: string;
+  inputMode: I;
+  resultMode: R;
+  handleInputChange: (newInput: string) => void;
+  handleInputModeChange: (newMode: I) => void;
+  handleResultModeChange: (newMode: R) => void;
+}
 
 export function useConverterState<I, R>(
   defaultInputMode: I,
   defaultResultMode: R,
   converter: (input: string, inputMode: I, resultMode: R) => string | null
-) {
+): ConverterStateHook<I, R> {
   const [input, setInput] = useState('');
   const [result, setResult] = useState('');
   const [inputMode, setInputMode] = useState(defaultInputMode);
