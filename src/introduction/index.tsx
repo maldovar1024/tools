0b406532208aaccc776cb519425b5fb91b9ae530
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2020-10-05 20:51:05 +0800

    feat: 给 markdown 添加 GitHub 样式

--- src/introduction/index.tsx
+++ src/introduction/index.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 import ReactMarkdown from 'react-markdown';
 import readme from './introduction.md';
+import './github-md-style.css';
 
-const Introduction: FC = () => <ReactMarkdown source={readme} />;
+const Introduction: FC = () => (
+  <ReactMarkdown className="markdown-body" source={readme} />
+);
 
 export default Introduction;
