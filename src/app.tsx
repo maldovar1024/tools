9bf888e4c1484bccc1b034873004915cfe95c2f2
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-01-04 15:20:37 +0800

    feat: 使用 tab 实现切换页面

--- src/app.tsx
+++ src/app.tsx
@@ -1,40 +1,20 @@
-import { Layout, Menu } from 'antd';
+import { Tabs } from 'antd';
 import React from 'react';
-import { Link, Route, Switch, useLocation } from 'react-router-dom';
-import NotFound from './404';
 import './app.less';
 import { FormConverter } from './form-converter';
-import Introduction from './introduction';
 import { RadixConverter } from './radix-converter';
 
-const { Sider, Content } = Layout;
-const { Item } = Menu;
+const { TabPane } = Tabs;
 
 export default function App(): JSX.Element {
-  const location = useLocation();
   return (
-    <Layout className="App">
-      <Sider>
-        <Menu theme="dark" mode="inline" selectedKeys={[location.pathname]}>
-          <Item key="/">
-            <Link to="/">简介</Link>
-          </Item>
-          <Item key="/radix-converter">
-            <Link to="/radix-converter">进制转换</Link>
-          </Item>
-          <Item key="/form-converter">
-            <Link to="/form-converter">格式转换</Link>
-          </Item>
-        </Menu>
-      </Sider>
-      <Content>
-        <Switch>
-          <Route exact path="/" component={Introduction} />
-          <Route exact path="/radix-converter" component={RadixConverter} />
-          <Route exact path="/form-converter" component={FormConverter} />
-          <Route component={NotFound} />
-        </Switch>
-      </Content>
-    </Layout>
+    <Tabs defaultActiveKey="radix-converter" tabPosition="left" size="large">
+      <TabPane tab="进制转换" key="radix-converter">
+        <RadixConverter />
+      </TabPane>
+      <TabPane tab="格式转换" key="form-converter">
+        <FormConverter />
+      </TabPane>
+    </Tabs>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app';
 import './index.less';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/tools">
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
