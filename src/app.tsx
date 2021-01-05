cc89fa3c71f4b00b0db447af23a55756318d838d
Zhang Zhengyi <45653651+zhangzhengyi1024@users.noreply.github.com>
2021-01-05 16:21:10 +0800

    refactor: App 组件使用 FC 类型声明

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,19 @@
 import { Tabs } from 'antd';
-import React from 'react';
+import React, { FC } from 'react';
 import FormConverter from './form-converter';
 import RadixConverter from './radix-converter';
 
 const { TabPane } = Tabs;
 
-export default function App(): JSX.Element {
-  return (
-    <Tabs defaultActiveKey="radix-converter" tabPosition="left" size="large">
-      <TabPane tab="进制转换" key="radix-converter">
-        <RadixConverter />
-      </TabPane>
-      <TabPane tab="格式转换" key="form-converter">
-        <FormConverter />
-      </TabPane>
-    </Tabs>
-  );
-}
+const App: FC = () => (
+  <Tabs defaultActiveKey="radix-converter" tabPosition="left" size="large">
+    <TabPane tab="进制转换" key="radix-converter">
+      <RadixConverter />
+    </TabPane>
+    <TabPane tab="格式转换" key="form-converter">
+      <FormConverter />
+    </TabPane>
+  </Tabs>
+);
+
+export default App;
